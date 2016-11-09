343620e9f5d20bfb6635e9456484dd115efd3455
Singwai Chan <Singwai.Chan@live.com>
2016-11-08 21:17:52 -0500

    feat (class 1):

---
+++ src/prep/it/singwai/Main.java
@@ -0,0 +1,43 @@
+package prep.it.singwai;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /*
+     * Learn about data type: int, double, boolean, String
+	 * Learn about if statment
+	 * Learn about for loop
+	 * Learn about while loop
+	 * Bonus: function.
+	 *      Function with argument(s)
+	 *      Function with return value
+	 *
+	 * Understand scope
+	 */
+
+        //This is a single lin
+        // datatype variableName = value;
+        String name1 = "Singwai";
+        int age1 = 24;
+
+        System.out.println(name1);
+        System.out.println(age1);
+
+        name1 = "Jack";
+        age1 = 22;
+
+        System.out.println(name1);
+        System.out.println(age1);
+
+
+        String name1Copy = name1;
+        int age1Copy = age1;
+
+        System.out.println(name1Copy);
+        System.out.println(age1Copy);
+
+        for (int i = 0 ; i < 10 ; i ++){
+            System.out.print(name1Copy);
+        }
+    }
+}
---
+++ src/prep/it/singwai/Summation.java
@@ -0,0 +1,60 @@
+package prep.it.singwai;
+
+
+public class Summation {
+
+    /*
+        Here is a little reminder for the syntax
+        public static (return type) (function name) (argument list)
+     */
+
+    /**
+     * @param input : inclusive upper bounce of number that we are adding
+     *              ex: input = 4
+     *                  Result = 1 + 2 + 3 + 4
+     */
+    public static void addNumber(int input) {
+        System.out.println("Hello add number");
+
+        int result = 0;
+
+        //for (start value, comparison, change value){
+        for (int currentValue = 1; currentValue <= input; currentValue++) {
+            result = result + currentValue;
+        }
+
+        System.out.println("The result is " + result);
+    }
+
+
+    /**
+     * @param input : inclusive upper bounce of number that we are adding
+     *              ex: input = 6
+     *                  Result = 1 + 3 + 5
+     *
+     *                  input = 15
+     *                  Result = 1+3+5+7+9+11+13+15 (Inclusive)
+     */
+    public static void addOddNumber(int input) {
+        System.out.println("Hello add odd number");
+
+        int result = 0;
+
+        //for (start value, comparison, change value){
+        for (int currentValue = 1; currentValue <= input; currentValue = currentValue + 2) {
+            result = result + currentValue;
+        }
+
+        System.out.println("The result is " + result);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+        addNumber(10);
+        addNumber(5);
+        addNumber(100);
+        addNumber(1000);
+        addOddNumber(10);
+    }
+}
