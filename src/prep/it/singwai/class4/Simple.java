a58cf4e71845192cd2c797c021980a4843d7896a
Singwai Chan <Singwai.Chan@live.com>
2016-11-26 10:21:57 -0500

    feat (class 4 start):

---
+++ src/prep/it/singwai/class4/Simple.java
@@ -0,0 +1,41 @@
+package prep.it.singwai.class4;
+
+public class Simple {
+
+    //Sub-String
+    //char array
+    //Vending machin
+    //Rot-13
+    //https://en.wikipedia.org/wiki/ROT13
+
+    public static void printStringAsCharArray(String in) {
+        char[] chars = in.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i] + " ");
+        }
+        System.out.print("\n");
+    }
+
+    public static void printAsciiInputFromNumber (int in){
+        if (in > 127 || in < 0){
+            System.out.println("Bad input");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Happy Halloween!";
+        String s2 = "Happy Thanksgiving!";
+        printStringAsCharArray(s1);
+        System.out.println(s1);
+
+        printStringAsCharArray(s2);
+        System.out.println(s2);
+
+
+        String sub1 = s1.substring(0, 5);
+        System.out.println(sub1);
+
+    }
+}
