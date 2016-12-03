313b20ba59adc36eb8d7d09d37d40d74b9880085
Singwai Chan <Singwai.Chan@live.com>
2016-12-03 11:49:54 -0500

    feat (class 5 calculator, review and post man):

--- src/prep/it/singwai/class5/Calculator.java
+++ src/prep/it/singwai/class5/Calculator.java
@@ -1,8 +1,8 @@
 package prep.it.singwai.class5;
 
-/**
- * Created by Personal on 11/27/16.
- */
+
+import java.util.Scanner;
+
 public class Calculator {
 
     public static final String OPERATION_TYPE = "OPERATION_TYPE";
@@ -15,4 +15,71 @@ public class Calculator {
     //Mod
     //Percentage
     //Exponential
+
+    public static int askOperation() {
+        System.out.println("Which operation would you like to perform?");
+        System.out.println("1:" + OPERATION_TYPE_ADD);
+        System.out.println("2:" + OPERATION_TYPE_SUBTRACT);
+        System.out.println("3:" + OPERATION_TYPE_MUTIPLTY);
+        System.out.println("4:" + OPERATION_TYPE_DIVIDE);
+
+        Scanner myScanner = new Scanner(System.in);
+        int result = myScanner.nextInt();
+
+        if (isValidOperation(result)) {
+            return result;
+        } else {
+            return askOperation();
+        }
+    }
+
+    public static boolean isValidOperation(int input) {
+        return input >= 1 && input <= 4;
+    }
+
+    public static boolean isZero(double input) {
+        return input == 0;
+    }
+
+    public static double askOperand() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter your input");
+        return myScanner.nextDouble();
+    }
+
+
+    public static void main(String[] args) {
+        double left;
+        double right;
+        double result;
+        int myOperatonType = askOperation();
+        left = askOperand();
+        right = askOperand();
+
+        System.out.println("Operation Type is " + myOperatonType);
+        System.out.println("left is " + left);
+        System.out.println("right is " + right);
+
+        if (myOperatonType == 1) {
+            result = left + right;
+        } else if (myOperatonType == 2) {
+            result = left - right;
+        } else if (myOperatonType == 3) {
+            result = left * right;
+        } else {
+            if (isZero(right)) {
+                //print not possible message
+                System.out.println("Right operand can't be 0");
+                result = -1;
+            } else {
+                result = left / right;
+            }
+        }
+
+        if (isZero(right) && myOperatonType == 4) {
+            System.out.println("result is unknown");
+        } else {
+            System.out.println("result is " + result);
+        }
+    }
 }
--- src/prep/it/singwai/class5/Simple.java
+++ src/prep/it/singwai/class5/Simple.java
@@ -1,12 +1,35 @@
 package prep.it.singwai.class5;
 
-/**
- * Created by Personal on 11/27/16.
- */
+
+import java.util.Date;
+import java.util.Scanner;
+
 public class Simple {
-
-
     public static void main(String[] args) {
+        //Excerise 1: Hello Person
+        //Ask Person's name and say hello to him/her.
 
+        //Bonus: Tell the person the current EST time.
+        //if person's name is loser
+        //Tell him he is not welcome.
+
+        //Write a for loop that will greet the person X time, depending on the length on the person's name
+        //Hint need scanner
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        Date date = new Date();
+
+        System.out.println("Hello, what is your name?");
+        name = scanner.nextLine();
+
+        for (int i = 0; i < name.length(); i++) {
+            if (!name.equalsIgnoreCase("loser")) {
+                System.out.println("Hello, " + name);
+            } else {
+                System.out.print("You are not welcome.");
+            }
+        }
+
+        System.out.println("The current time is " + date.toString());
     }
 }
